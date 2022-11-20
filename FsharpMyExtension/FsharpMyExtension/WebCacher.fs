f9f08b1f5a059c85382a69d891fb675952666fe9
gretmn102 <erlgreywatson@gmail.com>
2022-11-20 17:04:35 +0200

    feat: `WebCacher.gets`

--- FsharpMyExtension/FsharpMyExtension/WebCacher.fs
+++ FsharpMyExtension/FsharpMyExtension/WebCacher.fs
@@ -30,3 +30,44 @@ module WebCacher =
                 Ok(data, Some webCacher)
             | Left errMsg ->
                 Error errMsg
+
+    let gets reqf dataMapper urls (webCacher: WebCacher<'Data>) =
+        urls
+        |> Seq.map (fun url ->
+            match Map.tryFind url webCacher.Cache with
+            | Some data ->
+                async {
+                    return url, Ok {| IsNew = false; Data = data |}
+                }
+            | None ->
+                async {
+                    let! x = WebDownloader.tryGet reqf url
+
+                    match x with
+                    | Right data ->
+                        return url, Ok {| IsNew = true; Data = dataMapper data |}
+                    | Left errorValue ->
+                        return url, Error errorValue
+                }
+        )
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> fun results ->
+            let webCacher =
+                results
+                |> Array.fold
+                    (fun webCacher (url, res) ->
+                        match res with
+                        | Ok x ->
+                            if x.IsNew then
+                                { webCacher with
+                                    Cache = Map.add url x.Data webCacher.Cache
+                                }
+                            else
+                                webCacher
+                        | Error _ ->
+                            webCacher
+                    )
+                    webCacher
+
+            results, webCacher
