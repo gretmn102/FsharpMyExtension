459c62dd6a0a7749341ac105f37272fe28fa6c57
gretmn102 <erlgreywatson@gmail.com>
2022-12-21 20:24:04 +0200

    test: Bson

---
+++ Test/Test/JsonTests.fs
@@ -0,0 +1,14 @@
+module JsonTests
+open Fuchu
+open FsharpMyExtension
+
+type T = { Id: string; Y: string }
+
+[<Tests>]
+let bsonSerializerTests =
+    testList "bsonSerializerTests" [
+        testCase "base" <| fun () ->
+            let input = { Id = "SimpleQuizUI"; Y = "2000" }
+            let act: T = Bson.deserializeBytes (Bson.serialize input)
+            Assert.Equal("", input, act)
+    ]
