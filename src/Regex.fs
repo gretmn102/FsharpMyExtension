22de2aa381d9dd26896864f071db85a23a8d561b
gretmn102 <erlgreywatson@gmail.com>
2023-04-28 21:57:56 +0300

    feat: add `Regex.create` , `Regex.getMatches`

--- src/Regex.fs
+++ src/Regex.fs
@@ -1,6 +1,18 @@
 [<RequireQualifiedAccessAttribute>]
 module FsharpMyExtension.Regex
 
+let create (pattern: string) =
+    System.Text.RegularExpressions.Regex(pattern)
+
+let getMatches input (r: System.Text.RegularExpressions.Regex) =
+    r.Matches input
+    |> Seq.cast<System.Text.RegularExpressions.Match>
+    |> Seq.map (fun x ->
+        x.Groups
+        |> Seq.cast<System.Text.RegularExpressions.Group>
+        |> Seq.map (fun x -> x.Value)
+    )
+
 /// возвращает группу вхождений, кроме первой
 let matchOther pattern =
     let r = System.Text.RegularExpressions.Regex(pattern)
