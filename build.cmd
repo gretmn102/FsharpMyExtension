82e95d868bfd4fb2bc286770f1d8fc04b2d8764e
gretmn102 <erlgreywatson@gmail.com>
2023-08-30 13:15:09 +0300

    build: add `PAKET_SKIP_RESTORE_TARGETS` to `build.cmd` and `build.sh`

--- build.cmd
+++ build.cmd
@@ -2,4 +2,6 @@ cls
 
 dotnet tool restore
 dotnet paket install
+SET PAKET_SKIP_RESTORE_TARGETS=true
 dotnet fake build %*
+SET PAKET_SKIP_RESTORE_TARGETS=
--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 dotnet tool restore
 dotnet paket install
+PAKET_SKIP_RESTORE_TARGETS=true
 dotnet fake build $@
+PAKET_SKIP_RESTORE_TARGETS=
