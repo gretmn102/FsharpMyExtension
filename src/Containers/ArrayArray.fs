b530020067b3bfb7db5aa2ba095afe854ba3e102
gretmn102 <erlgreywatson@gmail.com>
2023-12-06 05:57:35 +0200

    feat: add abbreviated type `ArrayArray`

--- src/Containers/ArrayArray.fs
+++ src/Containers/ArrayArray.fs
@@ -1,32 +1,36 @@
-[<RequireQualifiedAccessAttribute>]
-module FsharpMyExtension.ArrayArray
+namespace FsharpMyExtension
 
-let columnIsEmpty columnIndex isEmpty (xss: 'a [] []) =
-    xss
-    |> Array.forall (fun xs ->
-        isEmpty xs.[columnIndex]
-    )
+type 'a ArrayArray = 'a [] []
 
-let trimLeft isEmpty (xss: 'a [] []) =
-    let columnIsEmpty columnIndex =
-        columnIsEmpty columnIndex isEmpty xss
-
-    let width = xss.[0].Length
-
-    let leftMost =
-        let rec apply x =
-            if x < width && columnIsEmpty x then
-                apply (x + 1)
-            else
-                x
-        apply 0
-
-
-    let diff = width - leftMost
-
-    xss
-    |> Array.map (fun xs ->
-        Array.init diff (fun i ->
-            xs.[leftMost + i]
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module ArrayArray =
+    let columnIsEmpty columnIndex isEmpty (xss: 'a ArrayArray) =
+        xss
+        |> Array.forall (fun xs ->
+            isEmpty xs.[columnIndex]
         )
-    )
+
+    let trimLeft isEmpty (xss: 'a ArrayArray) =
+        let columnIsEmpty columnIndex =
+            columnIsEmpty columnIndex isEmpty xss
+
+        let width = xss.[0].Length
+
+        let leftMost =
+            let rec apply x =
+                if x < width && columnIsEmpty x then
+                    apply (x + 1)
+                else
+                    x
+            apply 0
+
+
+        let diff = width - leftMost
+
+        xss
+        |> Array.map (fun xs ->
+            Array.init diff (fun i ->
+                xs.[leftMost + i]
+            )
+        )
