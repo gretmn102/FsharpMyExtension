3573ac4202bf41f4f5f63088b2feae0e0a3c412e
gretmn102 <erlgreywatson@gmail.com>
2023-12-16 04:03:26 +0200

    refact: move `LazyListZipper` to `Collections/LazyListZipper.fs`

---
+++ src/Collections/LazyListZipper.fs
@@ -0,0 +1,53 @@
+module FsharpMyExtension.Collections.LazyListZipper
+open FsharpMyExtension.ListZipper
+open FsharpMyExtension.Either
+open FsharpMyExtension.LazyList
+
+type LazyListZipper<'Error, 'a> =
+    {
+        SrcList: Lazy<LazyList<Either<'Error, 'a>>>
+        State: ListZ<'a>
+    }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module LazyListZipper =
+    type 'Error NextResult =
+        | EndOfList
+        | Error of 'Error
+
+    let create x src =
+        {
+            SrcList = src
+            State = ListZ.singleton x
+        }
+
+    let hole (llz: LazyListZipper<'Error,_>) =
+        ListZ.hole llz.State
+
+    let next (llz: LazyListZipper<'Error,_>) =
+        match ListZ.next llz.State with
+        | Some lz ->
+            { llz with
+                State = lz }
+            |> Right
+        | None ->
+            match llz.SrcList.Value with
+            | Cons(x, xs) ->
+                match x with
+                | Right x ->
+                    { llz with
+                        SrcList = lazy xs.Value
+                        State =
+                            ListZ.insertAfter x llz.State }
+                    |> Right
+                | Left x -> Left (Error x)
+            | Empty -> Left EndOfList
+
+    let prev (llz: LazyListZipper<'Error,_>) =
+        match ListZ.prev llz.State with
+        | Some lz ->
+            { llz with
+                State = lz }
+            |> Some
+        | None -> None
