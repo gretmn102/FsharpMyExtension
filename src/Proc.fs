8bc8be2f24a2408a7d871256afe810090315c501
gretmn102 <erlgreywatson@gmail.com>
2023-12-19 06:52:24 +0200

    refact: move `Proc` to `FsharpMyExtension` namespace

--- src/Proc.fs
+++ src/Proc.fs
@@ -1,16 +1,17 @@
-module Proc
+module FsharpMyExtension.Proc
+
 /// запускает процесс и ждет его завершения.
-let startProc dataReceived progPath args = 
+let startProc dataReceived progPath args =
     let startInfo = System.Diagnostics.ProcessStartInfo()
     startInfo.FileName <- progPath
     startInfo.Arguments <- args
-    
+
     startInfo.UseShellExecute <- false
     startInfo.RedirectStandardOutput <- true
 
     use proc = new System.Diagnostics.Process()
     proc.EnableRaisingEvents <- true
-    
+
     proc.OutputDataReceived.AddHandler(
         System.Diagnostics.DataReceivedEventHandler(
             fun _ args -> dataReceived args.Data
@@ -21,9 +22,11 @@ let startProc dataReceived progPath args =
     proc.BeginOutputReadLine()
     proc.WaitForExit()
     proc.ExitCode
+
 /// Выводит на консоль
 let startProcSimple path args =
     startProc (fun e -> printfn "%s" e) path args
+
 /// Выводит на консоль и одновременно в `string`
 let startProcString path args =
     let drivenOutput = new System.Text.StringBuilder()
