69c020287f9115a7837d541d9a4da5197a1b6ab4
gretmn102 <erlgreywatson@gmail.com>
2024-05-09 23:49:07 +0300

    refactor(tests): move `OptionTests` from `Tests.fs` to `Containers/Option.Tests.fs`

---
+++ tests/Containers/Option.Tests.fs
@@ -0,0 +1,26 @@
+module FsharpMyExtension.Containers.Option.Tests
+open Fuchu
+
+module Seq =
+    let seqOpt = FsharpMyExtension.Collections.Seq.seqOpt
+
+    [<Tests>]
+    let seqOptTest =
+        testList "seqOptTest" [
+            testCase "base case" <| fun () ->
+                Assert.Equal("", Some [], seqOpt Seq.empty)
+            testCase "many Some case" <| fun () ->
+                let xs = [0..9]
+                Assert.Equal("", Some xs, List.map Some xs |> seqOpt)
+            testCase "has None case" <| fun () ->
+                let xs = seq{ yield Some 1; yield None; yield Some 3 }
+                Assert.Equal("", None, seqOpt xs)
+            testCase "lazyness test" <| fun () ->
+                let counter = ref 0
+                let xs = seq{
+                    yield Some 1; incr counter;
+                    yield None; incr counter;
+                    yield Some 3; incr counter; }
+                seqOpt xs |> ignore
+                Assert.Equal("", 1, !counter)
+        ]
--- tests/Test.fs
+++ tests/Test.fs
@@ -1,40 +1,5 @@
 open Fuchu
 
-module OptionTests =
-    // module Option =
-    //     [<Tests>]
-    //     let ofNullTest =
-    //         let ofNull = FsharpMyExtension.Option.Option.ofNull
-    //         testList "ofNullTest" [
-    //             testCase "null case" <| fun () ->
-    //                 Assert.Equal("", None, ofNull null)
-    //             testCase "some case" <| fun () ->
-    //                 let x = 1
-    //                 Assert.Equal("", Some x, ofNull( box x) |> Option.map unbox)
-    //        ]
-    module Seq =
-        let seqOpt = FsharpMyExtension.Collections.Seq.seqOpt
-        [<Tests>]
-        let seqOptTest =
-            testList "seqOptTest" [
-                testCase "base case" <| fun () ->
-                    Assert.Equal("", Some [], seqOpt Seq.empty)
-                testCase "many Some case" <| fun () ->
-                    let xs = [0..9]
-                    Assert.Equal("", Some xs, List.map Some xs |> seqOpt)
-                testCase "has None case" <| fun () ->
-                    let xs = seq{ yield Some 1; yield None; yield Some 3 }
-                    Assert.Equal("", None, seqOpt xs)
-                testCase "lazyness test" <| fun () ->
-                    let counter = ref 0
-                    let xs = seq{
-                        yield Some 1; incr counter;
-                        yield None; incr counter;
-                        yield Some 3; incr counter; }
-                    seqOpt xs |> ignore
-                    Assert.Equal("", 1, !counter)
-           ]
-
 [<EntryPoint>]
 let main arg =
     defaultMainThisAssembly arg
