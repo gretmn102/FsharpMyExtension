06e48e6b4f2fba363979a5040712b45f2b99ccb3
gretmn102 <erlgreywatson@gmail.com>
2025-06-27 15:55:48 +0300

    feat: add `writeFile`

--- src/IO/MemoryFileSystem.fs
+++ src/IO/MemoryFileSystem.fs
@@ -13,3 +13,49 @@ let rec create (pathFragments: string list) content =
         | pathFragment::pathFragments ->
             Map [pathFragment, create pathFragments content]
     )
+
+[<RequireQualifiedAccess>]
+type WriteFileError =
+    | IsDirectory
+    | PathFragmentsIsEmpty
+    | NotImplementedYet
+
+let writeFile
+    (pathFragments: string list)
+    content
+    (fileSystem: MemoryFileSystem)
+    : Result<MemoryFileSystem, WriteFileError> =
+    let rec loop pathFragments fileSystem =
+        match pathFragments with
+        | [fileName] ->
+            match fileSystem with
+            | Directory dir ->
+                match Map.tryFind fileName dir with
+                | None | Some (File _) ->
+                    Map.add fileName (File content) dir
+                    |> Directory
+                    |> Ok
+                | Some (Directory _) ->
+                    Error WriteFileError.IsDirectory
+            | File _ ->
+                Map.add fileName (File content) Map.empty
+                |> Directory
+                |> Ok
+        | pathFragment::restPathFragments ->
+            match fileSystem with
+            | Directory dir ->
+                match Map.tryFind pathFragment dir with
+                | None | Some (File _) ->
+                    Map.add pathFragment (create restPathFragments content) dir
+                    |> Directory
+                    |> Ok
+                | Some (Directory _ as subDir) ->
+                    loop restPathFragments subDir
+                    |> Result.map (fun x ->
+                        Directory (Map.add pathFragment x dir)
+                    )
+            | File _ ->
+                failwith "не должно случиться?"
+        | [] ->
+            Error WriteFileError.PathFragmentsIsEmpty
+    loop pathFragments fileSystem
--- tests/IO/MemoryFileSystem.Tests.fs
+++ tests/IO/MemoryFileSystem.Tests.fs
@@ -1,16 +1,110 @@
 module FsharpMyExtension.IO.MemoryFileSystem.Tests
 open Fuchu
 
+open Helpers
+
 [<Tests>]
 let ``IO.MemoryFileSystem.create`` =
     testList "IO.MemoryFileSystem.create" [
         testCase "base case" <| fun () ->
-            Assert.Equal(
-              "",
-              Directory (
-                  Map ["discord", Directory (
-                      Map ["users", Directory (
-                          Map ["lumi.md", File "Парень по имени Lumi"])])]),
-              create ["discord"; "users"; "lumi.md"] "Парень по имени Lumi"
-            )
+            Expect.equal
+                (create ["discord"; "users"; "lumi.md"] "Парень по имени Lumi")
+                (Directory (
+                    Map ["discord", Directory (
+                        Map ["users", Directory (
+                            Map ["lumi.md", File "Парень по имени Lumi"])])]))
+                ""
+    ]
+
+[<Tests>]
+let ``IO.MemoryFileSystem.writeFile`` =
+    testList "IO.MemoryFileSystem.writeFile" [
+        testCase "create and write file in empty directory" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["lumi.md"] "Парень по имени Lumi"
+                    (Directory Map.empty))
+                (Ok <| Directory (Map [
+                    "lumi.md", File "Парень по имени Lumi"
+                ]))
+                ""
+        testCase "create and write file in not empty directory" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["lumi.md"] "Парень по имени Lumi"
+                    (Directory (Map ["somefile", File "some content"])))
+                (Ok <| Directory (Map [
+                    "lumi.md", File "Парень по имени Lumi"
+                    "somefile", File "some content"
+                ]))
+                ""
+        testCase "rewrite one directory file" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["lumi.md"] "Парень по имени Lumi"
+                    (Directory (Map ["lumi.md", File "empty"])))
+                (Ok <| Directory (Map [
+                    "lumi.md", File "Парень по имени Lumi"
+                ]))
+                ""
+        testCase "rewrite just file" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["lumi.md"] "Парень по имени Lumi"
+                    (File "empty"))
+                (Ok <| Directory (Map [
+                    "lumi.md", File "Парень по имени Lumi"
+                ]))
+                ""
+        testCase "rewrite one subdirectory file" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["users"; "lumi.md"] "Парень по имени Lumi"
+                    (Directory (Map [
+                        "users", Directory (Map [
+                            "lumi.md", File "empty"
+                        ])
+                    ]))
+                )
+                (Ok <| Directory (Map [
+                    "users", Directory (Map [
+                        "lumi.md", File "Парень по имени Lumi"
+                    ])
+                ]))
+                ""
+        testCase "create discord/users/lumi.md in discord/index.md" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["discord"; "users"; "lumi.md"] "Парень по имени Lumi"
+                    (Directory (Map [
+                        "discord", Directory (Map [
+                            "index.md", File ""
+                        ])
+                    ]))
+                )
+                (Ok <| Directory (Map [
+                    "discord", Directory (Map [
+                        "index.md", File ""
+                        "users", Directory (Map [
+                            "lumi.md", File "Парень по имени Lumi"
+                        ])
+                    ])
+                ]))
+                ""
+        testCase "create discord/users/lumi.md in discord" <| fun () ->
+            Expect.equal
+                (writeFile
+                    ["discord"; "users"; "lumi.md"] "Парень по имени Lumi"
+                    (Directory (Map [
+                        "discord", Directory Map.empty
+                    ]))
+                )
+                (Ok <| Directory (Map [
+                    "discord", Directory (Map [
+                        "users", Directory (Map [
+                            "lumi.md", File "Парень по имени Lumi"
+                        ])
+                    ])
+                ]))
+                ""
     ]
