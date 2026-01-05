335ca193ce007fab8a0f4540955bd0d93d288ebc
gretmn102 <erlgreywatson@gmail.com>
2026-01-05 20:26:37 +0200

    test(Serialization.DataFormats.Json): option serialize
    Closes: #3

--- tests/Serialization/DataFormats/Json.Tests.fs
+++ tests/Serialization/DataFormats/Json.Tests.fs
@@ -11,3 +11,48 @@ let bsonSerializerTests =
             let act: T = Bson.deserializeBytes (Bson.serialize input)
             Assert.Equal("", input, act)
     ]
+
+
+module SerializeOptionTests =
+    type TypeWithOption = {
+        Opt: int option
+    }
+
+    type TypeWithOptions = TypeWithOption list
+
+    let value: TypeWithOptions = [
+        { Opt = None }
+        { Opt = Some 10 }
+    ]
+
+    let jsonExp =
+        String.concat "\r\n" [
+            "["
+            "  {"
+            "    \"Opt\": null"
+            "  },"
+            "  {"
+            "    \"Opt\": 10"
+            "  }"
+            "]"
+        ]
+
+    [<Tests>]
+    let ``SerializeOption.ser`` =
+        testList "SerializeOption.ser" [
+            testCase "base" <| fun () ->
+                let act = FSharpJsonType.SerializeOption.ser value
+
+                Assert.Equal("", jsonExp, act)
+        ]
+
+    [<Tests>]
+    let ``SerializeOption.des`` =
+        testList "SerializeOption.des" [
+            testCase "base" <| fun () ->
+                let exp = value
+                let act: TypeWithOptions =
+                    FSharpJsonType.SerializeOption.des jsonExp
+
+                Assert.Equal("", exp, act)
+        ]
